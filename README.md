7fe6bd1c2df75a2668ed72e8b9d494abdee0c41a
ywx-stack <59912014+ywx-stack@users.noreply.github.com>
2022-04-12 09:32:38 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,83 @@
+<h1 align="center" style="border-bottom: none">
+  bupt-ncov-report-action
+</h1>
+
+<p align="center">
+  使用 GitHub Actions 自动填报北邮 COVID-19 疫情信息。
+</p>
+
+<p align="center">
+  <a href="https://github.com/imtsuki/bupt-ncov-report-action"><img src="https://img.shields.io/github/stars/imtsuki/bupt-ncov-report-action?style=social" alt="GitHub Stars"></a>
+  <a href="https://github.com/imtsuki/bupt-ncov-report-action"><img src="https://img.shields.io/github/forks/imtsuki/bupt-ncov-report-action?style=social" alt="GitHub Forks"></a>
+</p>
+
+![Telegram](img/telegram.jpg)
+
+这个 Action 会自动在北京时间的每天 8:00 AM 进行填报。
+
+为了防止网络波动造成的失败，会间隔十分钟尝试六次。
+
+旧版脚本用户（2022-03-31 之前），请更新以支持最新的 OAuth 登录。
+
+## 使用方法
+
+### 方法一（Use this template）
+
+最简单的方法，是点击上方绿色的 **Use this template**，使用这个模板创建你自己的 Repository：
+
+然后，在你自己仓库的 Settings 的 Secrets 中设置以下信息：
+
+- `BUPT_USERNAME`: 你用来登录的学号；
+- `BUPT_PASSWORD`: 你用来登录的密码。
+
+这种方法的缺点是无法同步上游的更新。
+
+### 方法二（Fork）
+
+**Use this template** 并不会自动追踪该仓库的后续更新。
+
+第二种方法是，点击右上角的 **Fork** 按钮来创建自己的 Repository。注意在这种情况下，你可能需要在 Settings 中手动开启 GitHub Actions 的运行，因为在 Fork 的仓库中，这个开关会默认关闭。
+
+![Actions Setting](img/actions-setting.png)
+
+若需要更新脚本，可以点击页面上方的 **Fetch upstream** 按钮：
+
+![Fetch Upstream](img/fetch-upstream-drop-down.png)
+
+然后点击 **Fetch and Merge**：
+
+![Fetch and Merge](img/fetch-and-merge-button.png)
+
+## 检查结果
+
+无需任何设置。如果运行失败，GitHub 会向你的邮箱发送一封邮件。
+
+如果你更改了设置，想手动重新运行，可以点进上方的 Actions 栏，点击 Re-run Jobs 来重新运行。
+
+### Telegram Bot
+
+如果你知道怎么使用 Telegram Bot，则可以额外设置如下的 Secrets 来用 Bot 给你发送结果：
+
+- `TG_BOT_TOKEN`: 你的 Bot 的 Token；
+- `TG_CHAT_ID`: 你和 Bot 的 Chat ID。
+
+### `Timeout awaiting 'request' for 2000ms` 错误
+
+这一般是 GitHub Actions 连接学校服务器过程中网络不稳定导致的。代码中默认以 2000ms 的间隔重试 100 次（见 `src/index.ts`）。这种情况下，你可以手动重试，或更改重试的间隔和次数，或者将该脚本部署到 Gitee 等国内服务上。
+
+### 表单字段更新
+
+有时，填报系统中的表单字段会有更新。由于该脚本会获取前一天的填报内容并提交，在某些情况下可能会出现错误。此时，你需要手动填报一次。如果第二天依然出错，请开 issue。
+
+## 高级设置
+
+你可以在 `.github/workflows/main.yml` 中来设置每天运行的时间：
+
+```yml
+on:
+  schedule:
+    - cron: '0 0 * * *'
+```
+
+格式是标准的 cron 格式，第一个数字代表分钟，第二个数字代表小时。例如，`0 1 * * *` 表示在每天
+格林尼治时间的 1:00 AM，也就是在北京时间的 9:00 AM 自动运行。
---
+++ src/index.ts
@@ -0,0 +1,186 @@
+import * as core from "@actions/core";
+import got, { Got } from "got";
+import { CookieJar } from "tough-cookie";
+import TelegramBot from "node-telegram-bot-api";
+import { LoginForm, DailyReportForm, DailyReportResponse } from "./form.js";
+import { sleep, randomBetween } from "./utils.js";
+
+const LOGIN = "https://auth.bupt.edu.cn/authserver/login";
+const GET_REPORT = "https://app.bupt.edu.cn/ncov/wap/default/index";
+const POST_REPORT = "https://app.bupt.edu.cn/ncov/wap/default/save";
+const RETRY = 100;
+const TIMEOUT = 2000;
+
+async function login(
+    loginForm: LoginForm
+): Promise<Got> {
+    const cookieJar = new CookieJar();
+    const client = got.extend({
+        cookieJar,
+        retry: {
+            limit: RETRY,
+            methods: ["GET", "POST"],
+        },
+        timeout: {
+            request: TIMEOUT,
+        },
+        throwHttpErrors: false,
+    });
+
+    // get `execution` field, will be used in post form data
+    let response = await client.get(LOGIN);
+    const execution = response.body.match(/input name="execution" value=.*><input name="_eventId"/)?.[0]?.replace('input name="execution" value="', '')?.replace('"/><input name="_eventId"', '');
+
+    if (!execution) {
+        throw new Error(`parse execution field failed`);
+    }
+
+    // embed additional fields
+    loginForm = {
+        submit: "LOGIN",
+        type: "username_password",
+        _eventId: "submit",
+        execution,
+        ...loginForm,
+    }
+
+    // login now,
+    // we need the cookie, and that's it! do not follow redirects
+    response = await client.post(LOGIN, { form: loginForm, followRedirect: false });
+
+    if (response.statusCode != 302) {
+        throw new Error(`login 请求返回了 ${response.statusCode}，应是 302`);
+    }
+
+
+    return client;
+}
+
+async function getDailyReportFormData(
+    client: Got
+): Promise<DailyReportForm> {
+    const response = await client.get(GET_REPORT);
+    if (response.statusCode != 200) {
+        throw new Error(`getFormData 请求返回了 ${response.statusCode}`);
+    }
+    if (response.body.indexOf("登录") != -1) {
+        throw new Error("登录失败；请检查用户名与密码是否正确");
+    }
+    const newForm: DailyReportForm = JSON.parse(
+        /var def = (\{.+\});/.exec(response.body)?.[1] ?? ""
+    );
+    const oldForm: DailyReportForm = JSON.parse(
+        /oldInfo: (\{.+\}),/.exec(response.body)?.[1] ?? ""
+    );
+
+    if (oldForm.geo_api_info === undefined) {
+        throw new Error("昨天的信息不完整；请手动填报一天后继续使用本脚本");
+    }
+
+    const geo = JSON.parse(oldForm.geo_api_info);
+
+    // 前一天的地址
+    const province = geo.addressComponent.province;
+    let city = geo.addressComponent.city;
+    if (geo.addressComponent.city.trim() === "" && ["北京市", "上海市", "重庆市", "天津市"].indexOf(province) > -1) {
+        city = geo.addressComponent.province;
+    } else {
+        city = geo.addressComponent.city;
+    }
+    const area = geo.addressComponent.province + " "
+        + geo.addressComponent.city + " "
+        + geo.addressComponent.district;
+    const address = geo.formattedAddress;
+
+    Object.assign(oldForm, newForm);
+
+    // 覆盖昨天的地址
+    oldForm.province = province;
+    oldForm.city = city;
+    oldForm.area = area;
+    oldForm.address = address;
+
+    // 强制覆盖一些字段
+    // 是否移动了位置？否
+    oldForm.ismoved = "0";
+    // 不在同城原因？空
+    oldForm.bztcyy = "";
+    // 是否省份不合？否
+    oldForm.sfsfbh = "0";
+
+    return oldForm;
+}
+
+async function postDailyReportFormData(
+    client: Got,
+    formData: DailyReportForm
+): Promise<DailyReportResponse> {
+    const response = await client.post(POST_REPORT, { form: formData });
+    if (response.statusCode != 200) {
+        throw new Error(`postFormData 请求返回了 ${response.statusCode}`);
+    }
+    return JSON.parse(response.body);
+}
+
+(async (): Promise<void> => {
+    const loginForm: LoginForm = {
+        username: process.env["BUPT_USERNAME"],
+        password: process.env["BUPT_PASSWORD"]
+    }
+
+    if (!(!!loginForm.username && !!loginForm.password)) {
+        throw new Error("无法登录；请在仓库 Settings 的 Secrets 栏填写 BUPT_USERNAME 与 BUPT_PASSWORD");
+    }
+
+    console.log("用户登录中");
+
+    const client = await login(loginForm);
+
+    await sleep(randomBetween(2000, 4000));
+
+    console.log("正在获取前一天的疫情填报信息");
+
+    const formData = await getDailyReportFormData(client);
+
+    await sleep(randomBetween(2000, 4000));
+
+    console.log("正在提交今日疫情填报信息");
+
+    const reportReponse = await postDailyReportFormData(client, formData);
+
+    console.log(`今日填报结果：${reportReponse.m}`);
+
+    const chatId = process.env["TG_CHAT_ID"];
+    const botToken = process.env["TG_BOT_TOKEN"];
+
+    if (!!chatId && !!botToken && reportReponse.m !== "今天已经填报了") {
+        const bot = new TelegramBot(botToken);
+        await bot.sendMessage(
+            chatId,
+            `今日填报结果：${reportReponse.m}`,
+            { "parse_mode": "Markdown" }
+        );
+    }
+})().catch(err => {
+    const chatId = process.env["TG_CHAT_ID"];
+    const botToken = process.env["TG_BOT_TOKEN"];
+
+    if (!!chatId && !!botToken && err instanceof Error) {
+        const bot = new TelegramBot(botToken);
+        bot.sendMessage(
+            chatId,
+            `填报失败：\`${err.message}\``,
+            { "parse_mode": "Markdown" }
+        );
+        console.log(err);
+    } else {
+        throw err;
+    }
+}).catch(err => {
+    if (err instanceof Error) {
+        console.log(err.stack);
+        core.setFailed(err.message);
+    } else {
+        core.setFailed(err);
+    }
+});
